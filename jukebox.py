f7cb66a12d213324588c1adfd9085d9ea97ec6a1
KamilFerenc <kamilferenc91@gmail.com>
2018-11-14 14:20:14 +0100

    Add subclass ScrollBox to create and grid listbox and scrollbar

--- jukebox.py
+++ jukebox.py
@@ -2,6 +2,20 @@ import tkinter
 import sqlite3
 
 conn = sqlite3.connect("music.sqlite")
+
+
+class ScrollBox(tkinter.Listbox):
+
+    def __init__(self, window, **kwargs):
+        super().__init__(window, **kwargs)
+        self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, command=self.yview)
+
+    def grid(self, row, column, sticky="nsw", rowspan=1, columnspan=1, **kwargs):
+        super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, columnspan=columnspan, **kwargs)
+        self.scrollbar.grid(row=row, column=column, sticky="nse", rowspan=rowspan, columnspan=columnspan, **kwargs)
+        self["yscrollcommand"] = self.scrollbar.set
+
+
 mainWindow = tkinter.Tk()
 mainWindow.geometry("1024x768")
 mainWindow.title("Music DB Browser")
@@ -23,20 +37,32 @@ artistLabel = tkinter.Label(mainWindow, text="Artist").grid(row=0, column=0)
 albumsLabel = tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 songsLabel = tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
-# Artists Listbox
-artistList = tkinter.Listbox(mainWindow)
+# Artists Listbox + Scrollbar
+artistListbox = ScrollBox(mainWindow)
 # padx=(left_side_distance, right_side_distance)
-artistList.grid(row=1, column=0, sticky='nswe', rowspan=2, padx=(30, 0))
-artistList.config(border=2, relief="sunken")
+artistListbox.grid(row=1, column=0, sticky='nswe', rowspan=2, padx=(30, 0))
+artistListbox.config(border=2, relief="sunken")
 
-# Albums Listbox
-albumsListbox = tkinter.Listbox(mainWindow)
+# Albums Listbox + Scrollbar
+albumLV = tkinter.Variable(mainWindow)
+albumLV.set(("Choose an artist.",))
+albumsListbox = ScrollBox(mainWindow, listvariable=albumLV)
 albumsListbox.grid(row=1, column=1, sticky="nswe", padx=(30, 0))
 albumsListbox.config(border=2, relief="sunken")
 
-# Songs Listbox
-songsListbox = tkinter.Listbox(mainWindow)
+# Songs Listbox+ Scrollbar
+songsLV = tkinter.Variable(mainWindow)
+songsLV.set(("Choose an album.",))
+songsListbox = ScrollBox(mainWindow, listvariable=songsLV)
 songsListbox.grid(row=1, column=2, sticky="nswe", padx=(30, 0))
 songsListbox.config(border=2, relief="sunken")
 
+
+testlist = range(0, 100)
+albumLV.set(tuple(testlist))
+
+
 mainWindow.mainloop()
+
+print("Closing database connection.")
+conn.close()
